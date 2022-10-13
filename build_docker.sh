48d95e10dd8a6c7c777e011be8f9182a245e1ad5
sharnoff <sharnoff@neon.tech>
2022-10-13 12:09:27 -0700

    shell-escape, format build_docker.sh; use buildx

--- build_docker.sh
+++ build_docker.sh
@@ -7,39 +7,42 @@ REGISTRY=sergeyneon
 TAG=dev-2022-09-28
 
 echo virt-prerunner
-docker build \
+docker buildx build \
     --pull \
     -f build/virt-prerunner/Dockerfile \
-    -t ${REGISTRY}/virt-prerunner:${TAG} \
+    -t "${REGISTRY}/virt-prerunner:${TAG}" \
     .
 
 echo virt-controller
-docker build \
+docker buildx build \
     --pull \
     -f build/virt-controller/Dockerfile \
-    -t ${REGISTRY}/virt-controller:${TAG} \
-    --build-arg PRERUNNER_IMAGE=${REGISTRY}/virt-prerunner:${TAG} \
+    -t "${REGISTRY}/virt-controller:${TAG}" \
+    --build-arg PRERUNNER_IMAGE="${REGISTRY}/virt-prerunner:${TAG}" \
     .
 
 echo virt-daemon
-docker build \
+docker buildx build \
     --pull \
     -f build/virt-daemon/Dockerfile \
-    -t ${REGISTRY}/virt-daemon:${TAG} \
+    -t "${REGISTRY}/virt-daemon:${TAG}" \
     .
 
 echo push
-docker push ${REGISTRY}/virt-prerunner:${TAG}
-docker push ${REGISTRY}/virt-controller:${TAG}
-docker push ${REGISTRY}/virt-daemon:${TAG}
+docker push "${REGISTRY}/virt-prerunner:${TAG}"
+docker push "${REGISTRY}/virt-controller:${TAG}"
+docker push "${REGISTRY}/virt-daemon:${TAG}"
 
 LOCALBIN="$(pwd)/bin"
-SKAFFOLD=${LOCALBIN}/skaffold
+SKAFFOLD="${LOCALBIN}/skaffold"
 
-GOARCH=$(go env GOARCH)
-GOOS=$(go env GOOS)
+GOARCH="$(go env GOARCH)"
+GOOS="$(go env GOOS)"
 
-mkdir -p ${LOCALBIN}
-test -f ${SKAFFOLD} || curl -sLo ${SKAFFOLD} https://storage.googleapis.com/skaffold/releases/latest/skaffold-${GOOS}-${GOARCH} && chmod +x ${SKAFFOLD}
+mkdir -p "${LOCALBIN}"
+SKAFFOLD_URL="https://storage.googleapis.com/skaffold/releases/latest/skaffold-${GOOS}-${GOARCH}"
+test -f "${SKAFFOLD}" || curl -sLo "${SKAFFOLD}" "$SKAFFOLD_URL" && chmod +x "${SKAFFOLD}"
 
-PATH=${LOCALBIN}:${PATH} ${SKAFFOLD} render --offline=true --default-repo=${REGISTRY} --digest-source=tag --images virt-controller:${TAG},virt-daemon:${TAG} >virtink_${REGISTRY}.yaml
+PATH="${LOCALBIN}:${PATH}" "${SKAFFOLD}" render --offline=true --default-repo="${REGISTRY}" \
+    --digest-source=tag --images "virt-controller:${TAG},virt-daemon:${TAG}" \
+    >"virtink_${REGISTRY}.yaml"
